d34f8c12ebc32ae2630ba3fd28bbdd032bd6a24f
Evan Chu <echu1@ocdsb.ca>
2019-02-02 15:36:00 -0500

    turn scaling and starting to work on encoders

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -3,12 +3,14 @@ package frc.robot;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.DemandType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Joystick;
 
 import edu.wpi.first.wpilibj.CameraServer;
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.IterativeRobot;
 
 import org.opencv.core.Mat;
@@ -29,6 +31,7 @@ import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.videoio.VideoCapture;
 import org.opencv.videoio.VideoWriter;
 import org.opencv.videoio.Videoio;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 
 
 
@@ -40,14 +43,17 @@ public class Robot extends TimedRobot {
 
 	WPI_VictorSPX _rightMasterFront = new WPI_VictorSPX(1);
 	WPI_VictorSPX _rightMasterBack = new WPI_VictorSPX(2);
+
+	WPI_TalonSRX _intake = new WPI_TalonSRX(8);
   
 	Joystick _gamepad = new Joystick(0);
-
+	Encoder enc;
+	int distance;
 	edu.wpi.first.cameraserver.CameraServer server;
 
 	@Override
 	public void robotInit() {
-		
+		//_intake.getSensorCollection().setQuadraturePosition(0, 30);
 	}
 
 	@Override
@@ -75,10 +81,13 @@ public class Robot extends TimedRobot {
 		_leftMasterBack.setInverted(false);
 		_rightMasterFront.setInverted(true);
 		_rightMasterBack.setInverted(true);
+
+		_intake.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 1, 30);
+		_intake.getSensorCollection().setQuadraturePosition(0, 30);
 		
 		cameraInit();
-
-		System.out.println("This is Arcade Drive using Arbitrary Feed Forward.");
+		
+		//System.out.println("This is Arcade Drive using Arbitrary Feed Forward.");
 	}
 	
 	@Override
@@ -86,13 +95,34 @@ public class Robot extends TimedRobot {
 		
 		/* Gamepad processing */
 		double forward = -1 * _gamepad.getY();
-		
 		double turn = _gamepad.getX();		
-			
-		forward = Deadband(forward);
-		turn = Deadband(turn);
-		System.out.println(turn);
-		// System.out.println(turn);
+		
+		int count = _intake.getSelectedSensorPosition();
+
+		//System.out.println(count);
+		System.out.println(_intake.getSelectedSensorPosition());
+		if (_gamepad.getTrigger()) {
+			forward = Deadband(forward);
+			turn = Deadband(turn);
+			turn = Turn_Scale(forward, turn);
+		} else {
+			forward = Deadband_Scale(forward);
+			turn = Deadband_Scale(turn);
+			turn = Turn_Scale(forward, turn);
+		}
+
+		// if (_gamepad.getRawButton(3)) {
+		// 	if (_intake.getSelectedSensorPosition() >= -80){
+		// 		_intake.set(ControlMode.PercentOutput, -.1);
+		// 	}
+
+		// }
+
+		// if (_gamepad.getRawButton(4)) {
+		// 	if (_intake.getSelectedSensorPosition() <= 80){
+		// 		_intake.set(ControlMode.PercentOutput, .1);
+		// 	}
+		// }
 
 		/* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
 		_leftMasterFront.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, +turn);
@@ -101,6 +131,17 @@ public class Robot extends TimedRobot {
 		_rightMasterFront.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, -turn);
 		_rightMasterBack.set(ControlMode.PercentOutput, forward, DemandType.ArbitraryFeedForward, -turn);
 
+	}
+
+
+	
+	double Turn_Scale(double forwardValue, double turnValue) {
+		if (forwardValue > 0.5)
+			return turnValue *= (1 - forwardValue);
+		else if (forwardValue < -0.5)
+			return turnValue *= (1 + forwardValue);
+		
+		return turnValue;
 	}
 
 	/** Deadband 5 percent, used on the gamepad */
@@ -134,8 +175,10 @@ public class Robot extends TimedRobot {
 	public void cameraInit () {
 		server = edu.wpi.first.cameraserver.CameraServer.getInstance();
 		server.startAutomaticCapture(0);
-		server.startAutomaticCapture(1);		
+		server.startAutomaticCapture(1);	
 	}
+
+	
 
 }
 
